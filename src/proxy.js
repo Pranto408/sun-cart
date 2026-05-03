8c08e049bd4e4717a4c604ffcb7db0346674e362
Pranto408 <prantodatta408@gmail.com>
2026-05-04 02:38:10 +0600

    Proxy Update

--- src/proxy.js
+++ src/proxy.js
@@ -1,23 +1,42 @@
-import { headers } from 'next/headers'
-import { NextResponse } from 'next/server'
-import { NextRequest } from 'next/server'
-import { auth } from './lib/auth'
- 
-// This function can be marked `async` if using `await` inside
-export async function proxy(request) {
-    const session = await auth.api.getSession({
-        headers: await headers() // you need to pass the headers object.
-    });
-    if (!session) {
-        return NextResponse.redirect(new URL("/sign-in", request.url));
-    }
+// import { headers } from 'next/headers'
+// import { NextResponse } from 'next/server'
+// import { NextRequest } from 'next/server'
+// import { auth } from './lib/auth'
 
+// // This function can be marked `async` if using `await` inside
+// export async function proxy(request) {
+//     const session = await auth.api.getSession({
+//         headers: await headers() // you need to pass the headers object.
+//     });
+//     if (!session) {
+//         return NextResponse.redirect(new URL("/sign-in", request.url));
+//     }
 
+// }
+
+// // Alternatively, you can use a default export:
+// // export default function proxy(request: NextRequest) { ... }
+
+// export const config = {
+//   matcher: ['/profile','/products/:path'],
+// }
+
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
+import { auth } from "./lib/auth";
+
+export default async function proxy(request) {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    return NextResponse.redirect(new URL("/sign-in", request.url));
+  }
+
+  return NextResponse.next();
 }
- 
-// Alternatively, you can use a default export:
-// export default function proxy(request: NextRequest) { ... }
- 
+
 export const config = {
-  matcher: ['/profile','/products/:path'],
-}
+  matcher: ["/profile", "/products/:path*"],
+};
